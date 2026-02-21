8703355bc67db94eca42ef02673573286f75364b
jasparmetzger-dev <jaspar.metzger@web.de>
2026-02-21 13:57:03 +0100

    adding docker, railway deployment done

--- test_db.py
+++
@@ -1,7 +0,0 @@
-from app.database import engine, Base
-print("imported app.database")
-from app import models
-print("imported app.models")
-
-Base.metadata.create_all(bind=engine)
-print("Tables created successfully!")
